1a52a64bdb9165e43d4751ad38ab4b0c00cd87ad
Ruben Martirosyan <ruben@ruben.com>
2024-12-16 16:43:29 +0100

    Add Tests for Calc

---
+++ FileIoExamples.Business.Tests/CalcTests.cs
@@ -0,0 +1,86 @@
+﻿namespace FileIoExamples.Business.Tests
+{
+    public class CalcTests
+    {
+        private Calc _calc;
+
+        [Fact]
+        public void Add_Success()
+        {
+            //Arrange
+            _calc = new Calc();
+            const int a = 5;
+            const int b = 7;
+            const int expectedResult = 12;
+
+            //Act
+            int result = _calc.Add(a, b);
+
+            //Assert
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Fact]
+        public void Add_0value_Success()
+        {
+            //Arrange
+            _calc = new Calc();
+            const int a = 0;
+            const int b = 7;
+            const int expectedResult = 0;
+
+            //Act
+            int result = _calc.Add(a, b);
+
+            //Assert
+            Assert.Equal(expectedResult, result);
+        }
+
+
+        [Theory]
+        [InlineData(1, 3, 4)]
+        [InlineData(0, 3, 0)]
+        [InlineData(1, 0, 0)]
+        public void Add_AllCases_Success(int a, int b, int expectedResult)
+        {
+            //Arrange
+            _calc = new Calc();
+
+            //Act
+            int result = _calc.Add(a, b);
+
+            //Assert
+            Assert.Equal(expectedResult, result);
+        }
+
+
+        [Fact]
+        public void Add_NegativeA_ThrowsException()
+        {
+            //Arrange
+            _calc = new Calc();
+            const int a = -5;
+            const int b = 7;
+
+            //Act
+            //Assert
+            Assert.Throws<ArgumentOutOfRangeException>(() => _calc.Add(a, b));
+        }
+
+        [Fact]
+        public void Add_NegativeB_ThrowsException()
+        {
+            //Arrange
+            _calc = new Calc();
+            const int a = 5;
+            const int b = -7;
+
+            //Act
+            ArgumentOutOfRangeException ex = Assert.Throws<ArgumentOutOfRangeException>(() => _calc.Add(a, b));
+
+            //Assert
+            Assert.Null(ex.InnerException);
+            Assert.Equal("b", ex.ParamName);
+        }
+    }
+}
---
+++ FileIoExamples.Business/Calc.cs
@@ -0,0 +1,26 @@
+﻿namespace FileIoExamples.Business
+{
+    public class Calc
+    {
+        public int Add(int a, int b)
+        {
+            if (a < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(a));
+            }
+
+            if (b < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(b));
+            }
+
+            if (a == 0 || b == 0)
+            {
+                return 0;
+            }
+
+            int result = a + b;
+            return result;
+        }
+    }
+}
