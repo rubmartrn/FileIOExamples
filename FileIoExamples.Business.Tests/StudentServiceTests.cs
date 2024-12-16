51b4ec19b082c965597dc20f274bbb4515e5ce04
Ruben Martirosyan <ruben@ruben.com>
2024-12-16 16:59:27 +0100

    Add tests for student Service

---
+++ FileIoExamples.Business.Tests/StudentServiceTests.cs
@@ -0,0 +1,38 @@
+﻿namespace FileIoExamples.Business.Tests
+{
+    public class StudentServiceTests
+    {
+        private StudentService _service;
+
+        [Fact]
+        public void GetStudentsByUniversityName_Success()
+        {
+            //Arrange 
+            _service = new StudentService();
+            const string universityName = "University";
+            const string expectedStudentName = "Poghos";
+
+            //Act
+            var result = _service.GetStudentsByUniversityName(universityName);
+
+            //Arrange
+            var students = result.ToList();
+            Assert.NotEmpty(students);
+            Assert.Contains(expectedStudentName, students.Select(e => e.Name));
+        }
+
+        [Fact]
+        public void GetStudentsByUniversityName_MissingUniverityName_Success()
+        {
+            //Arrange 
+            _service = new StudentService();
+            const string missingUniversityName = "University1234";
+
+            //Act
+            var result = _service.GetStudentsByUniversityName(missingUniversityName);
+
+            //Arrange
+            Assert.Empty(result);
+        }
+    }
+}
---
+++ FileIoExamples.Business/StudentService.cs
@@ -0,0 +1,52 @@
+﻿using FileIOExamples.Business;
+
+namespace FileIoExamples.Business
+{
+    public class StudentService
+    {
+        List<Student> students = new List<Student>()
+        {
+            new Student()
+            {
+                Id = 7,
+                Name = "Poghos",
+                Address = "Yerevan",
+                UniversityName = "University",
+                Type = StudentType.Type1,
+                Date = DateTime.Now
+
+            },
+            new Student()
+            {
+                Id = 7,
+                Name = "Petros",
+                Address = "Yerevan",
+                UniversityName = "University",
+                Type = StudentType.Type3,
+                Date = DateTime.Now
+            },
+            new Student()
+            {
+                Id = 7,
+                Name = "Martiros",
+                Address = "Yerevan",
+                UniversityName = "University1",
+                Type = StudentType.Type2,
+                Date = DateTime.Now
+            },
+            new Student()
+            {
+                Id = 7,
+                Name = "John",
+                Address = "Yerevan",
+                Type = StudentType.Type1,
+                Date = DateTime.Now
+            },
+        };
+
+        public IEnumerable<Student> GetStudentsByUniversityName(string universityName)
+        {
+            return students.Where(e => e.UniversityName == universityName);
+        }
+    }
+}
