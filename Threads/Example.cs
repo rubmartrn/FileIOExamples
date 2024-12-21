9d5cdf40e88d28ed7e489c7d6487f650a40a6dcb
Ruben Martirosyan <ruben@ruben.com>
2024-12-21 11:09:44 +0100

    Add volatile

---
+++ Threads/Example.cs
@@ -0,0 +1,17 @@
+﻿namespace Threads
+{
+    internal class Example
+    {
+        public volatile bool isRunning = true;
+
+        public void Run()
+        {
+            while (isRunning)
+            {
+                Console.WriteLine("Run");
+            }
+
+            Console.WriteLine("Stop");
+        }
+    }
+}
--- Threads/Program.cs
+++ Threads/Program.cs
@@ -1,49 +1,21 @@
-﻿namespace Threads
+﻿using System.Text;
+
+namespace Threads
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            while (true)
-            {
-                Console.WriteLine(DateTime.Now);
-                Thread.Sleep(1000);
-                //Console.Clear();
-            }
-           
-            Thread thread1 = new Thread(CountUp);
-            thread1.Start();
-            thread1.Join();
-            thread1.Name = "Thread1";
+            Console.OutputEncoding = Encoding.UTF8;
 
-            Thread thread3 = new Thread(() => Count(650));
-            thread3.Start();
-
-            Thread thread2 = new Thread(CountDown);
-            thread2.Start();
-            Console.WriteLine("Hello, World!");
-        }
-
-        static void Count(int a)
-        {
-            Console.WriteLine($"thread3: {a}");
-        }
-
-        static void CountUp()
-        {
-            for (int i = 0; i <= 100; i++)
-            {
-                Thread.Sleep(1000);
-                Console.WriteLine($"Thread1: {i}");
-            }
-        }
-
-        static void CountDown()
-        {
-            for (int i = 100; i >= 0; i--)
-            {
-                Console.WriteLine($"Thread2: {i}");
-            }
+            Example example = new Example();
+            Thread t = new Thread(example.Run);
+            t.Start();
+            Console.WriteLine("Սեղմեք ինչ որ բան ստոպի համար");
+            Console.ReadKey();
+            example.isRunning = false;
+            Console.WriteLine(example.isRunning);
+            t.Join();
         }
     }
 }
