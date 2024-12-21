4492a0c89ed5e1a377cbc88ecde1c04981db3cb3
Ruben Martirosyan <ruben@ruben.com>
2024-12-21 10:51:34 +0100

    Add timer with thread sleep

--- Threads/Program.cs
+++ Threads/Program.cs
@@ -4,18 +4,36 @@
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.WriteLine(DateTime.Now);
+                Thread.Sleep(1000);
+                //Console.Clear();
+            }
+           
             Thread thread1 = new Thread(CountUp);
             thread1.Start();
             thread1.Join();
+            thread1.Name = "Thread1";
+
+            Thread thread3 = new Thread(() => Count(650));
+            thread3.Start();
+
             Thread thread2 = new Thread(CountDown);
             thread2.Start();
             Console.WriteLine("Hello, World!");
+        }
+
+        static void Count(int a)
+        {
+            Console.WriteLine($"thread3: {a}");
         }
 
         static void CountUp()
         {
             for (int i = 0; i <= 100; i++)
             {
+                Thread.Sleep(1000);
                 Console.WriteLine($"Thread1: {i}");
             }
         }
