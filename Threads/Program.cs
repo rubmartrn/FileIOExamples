f884d1b65806d5f621a289de6dd78a5ad086c5e2
Ruben Martirosyan <ruben@ruben.com>
2024-12-23 17:17:44 +0100

    Add Threadpool queue example

--- Threads/Program.cs
+++ Threads/Program.cs
@@ -9,42 +9,12 @@ internal class Program
     private static void Main(string[] args)
     {
         Console.OutputEncoding = Encoding.UTF8;
-        Thread t1 = new Thread(Thread1Run);
-        Thread t2 = new Thread(Thread2Run);
-
-        t1.Start();
-        t2.Start();
-
-        t1.Join();
-        t2.Join();
+        ThreadPool.QueueUserWorkItem(s =>
+        {
+            Console.WriteLine("Բարև");
+        });
 
         Console.WriteLine("Ավարտ");
-    }
-
-    static void Thread1Run()
-    {
-        lock (lock1)
-        {
-            Console.WriteLine("lock 1 from t1");
-            Thread.Sleep(1000);
-            lock (lock2)
-            {
-                Console.WriteLine("lock 2 from t1");
-            }
-        }
-    }
-
-    static void Thread2Run()
-    {
-        lock (lock2)
-        {
-            Console.WriteLine("lock 2 from t2");
-            Thread.Sleep(1000);
-            lock (lock1)
-            {
-                Console.WriteLine("lock 1 from t2");
-            }
-        }
     }
 }
 
