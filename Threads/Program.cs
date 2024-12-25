2e2d38b8a7ef8ef9794bf1baf3e97e39da8f7980
Ruben Martirosyan <ruben@ruben.com>
2024-12-25 17:26:40 +0100

    Add Write in file async

--- Threads/Program.cs
+++ Threads/Program.cs
@@ -7,12 +7,29 @@ internal class Program
     private static async Task Main(string[] args)
     {
         Console.OutputEncoding = Encoding.UTF8;
-        await Run();
+
+        await WriteAsync("a.txt", "Բարև Ձեզ");
+
+        string text = await ReadAsync("a.txt");
+        Console.WriteLine(text);
     }
 
-    static async Task Run()
+    static async Task WriteAsync(string filePath, string text)
     {
-        Console.WriteLine("Start");
-        throw new Exception("սխալ");
+        byte[] textBytes = Encoding.UTF8.GetBytes(text);
+
+        await using FileStream stream = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.None, 4096, useAsync: true);
+        await stream.WriteAsync(textBytes);
+    }
+
+    static async Task<string> ReadAsync(string filePath)
+    {
+
+        await using FileStream stream = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.None, 4096, useAsync: true);
+        byte[] textBytes = new byte[stream.Length];
+
+        await stream.ReadAsync(textBytes, 0, (int)stream.Length);
+        string result = Encoding.UTF8.GetString(textBytes);
+        return result;
     }
 }
