b2143f61cd280a8a4821c808f1b4d0177c5acd92
Ruben Martirosyan <ruben@ruben.com>
2024-12-25 16:25:28 +0100

    Add cancellation token

--- Threads/Program.cs
+++ Threads/Program.cs
@@ -8,29 +8,36 @@ internal class Program
     {
         Console.OutputEncoding = Encoding.UTF8;
 
-        Task.Run(() =>
+        CancellationTokenSource src = new CancellationTokenSource();
+        src.CancelAfter(5000);
+        try
         {
-            Console.Write("Բարև ");
-
-        }).ContinueWith((p) =>
-        {
-            Console.Write("Ձեզ ");
-        }).Wait();
-
-        Task[] tasks = new Task[10];
-
-        for (int i = 0; i < 10; i++)
-        {
-            tasks[i] = Test(i);
+            int a = Run(src.Token).Result;
+            Console.WriteLine(a);
         }
-
-        //Task.WaitAll(tasks);
-        Task.WaitAny(tasks);
+        catch (OperationCanceledException e)
+        {
+            Console.WriteLine("Գործողությունը չեղարկվել է");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            throw;
+        }
     }
 
-    static Task Test(int i)
+    static Task<int> Run(CancellationToken token)
     {
-        Console.WriteLine(i);
-        return Task.CompletedTask;
+        for (int i = 0; i < 10000; i++)
+        {
+            token.ThrowIfCancellationRequested();
+            //if (token.IsCancellationRequested)
+            //{
+            //    return Task.FromCanceled(token);
+            //}
+            Task.Delay(1000, token).Wait(token);
+            Console.WriteLine(i);
+        }
+        return Task.FromResult(4);
     }
 }
