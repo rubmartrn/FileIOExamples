166f5d1f693c9f2dc5a7b528e8f6020e7dc236d0
Ruben Martirosyan <ruben@ruben.com>
2025-01-08 17:30:32 +0100

    Add middlewares

---
+++ NewApi/Program.cs
@@ -0,0 +1,34 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.Use(async (context, next) =>
+{
+    await next.Invoke();
+});
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.Use(async (context, next) =>
+{
+    await next.Invoke();
+});
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+
+app.Run(async context =>
+{
+    await context.Response.WriteAsync("Barev");
+});
+
+app.Run();
