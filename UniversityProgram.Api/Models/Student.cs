38b552a5bd4a45073e63cc23deee81132c30ed69
Ruben Martirosyan <ruben@ruben.com>
2025-01-22 18:01:10 +0100

    Add EF

---
+++ UniversityProgram.Api/Models/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace UniversityProgram.Api.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+        public string Email { get; set; } = default!;
+    }
+}
---
+++ UniversityProgram.Api/StudentDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UniversityProgram.Api.Models;
+
+namespace UniversityProgram.Api
+{
+    public class StudentDbContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Student>()
+                .HasKey(e => e.Id);
+            modelBuilder.Entity<Student>().Property(e => e.Name).IsRequired();
+        }
+    }
+}
