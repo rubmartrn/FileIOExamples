84e90e69ee9eec9d6bf2e61d90bba092dd905fbd
Ruben Martirosyan <ruben@ruben.com>
2025-02-03 18:44:30 +0100

    Add validation examples

--- UniversityProgram.Api/Validators/LaptopAddModelValidator.cs
+++ UniversityProgram.Api/Validators/LaptopAddModelValidator.cs
@@ -1,11 +1,12 @@
 ﻿using FluentValidation;
+using Microsoft.EntityFrameworkCore;
 using UniversityProgram.Api.Models;
 
 namespace UniversityProgram.Api.Validators
 {
     public class LaptopAddModelValidator : AbstractValidator<LaptopAddModel>
     {
-        public LaptopAddModelValidator()
+        public LaptopAddModelValidator(StudentDbContext context)
         {
             RuleFor(e => e.Name)
                 .NotNull().WithMessage("Նեյմը պետք է նալ չլինի")
@@ -13,9 +14,10 @@ namespace UniversityProgram.Api.Validators
                 .MaximumLength(45).WithMessage("մաքսիմալ երկարությունը պետք է լինի 45 նիշ");
 
             RuleFor(e => e.StudentId)
-                .GreaterThanOrEqualTo(0).WithMessage("Ընտրեք ուսանողի Id-ն");
+                .GreaterThanOrEqualTo(0).WithMessage("Ընտրեք ուսանողի Id-ն")
+                .MustAsync(async (e, token) => await context.Students.AnyAsync(s => s.Id == e)).WithMessage("Տվյալ այդիով ուսանող չկա");
 
-            RuleFor(e => e.Cpu).SetValidator(new CpuAddModelValidator());
+            //RuleFor(e => e.Cpu).SetValidator(new CpuAddModelValidator());
         }
     }
 
@@ -23,7 +25,7 @@ namespace UniversityProgram.Api.Validators
     {
         public CpuAddModelValidator()
         {
-            RuleFor(e=>e.Name).NotNull().Must(e =>
+            RuleFor(e => e.Name).NotNull().Must(e =>
             {
                 foreach (var a in e)
                 {
@@ -31,7 +33,7 @@ namespace UniversityProgram.Api.Validators
                     {
                         return false;
                     }
-                } 
+                }
                 return true;
             }).WithMessage("Սիմվոլներ չեն կարելի");
 
