bba7ea86b7abea50c299de2899721a8c25fa5ba3
Ruben Martirosyan <ruben@ruben.com>
2025-02-08 11:22:54 +0100

    Add events in downloader

--- ConsoleTests/Program.cs
+++ ConsoleTests/Program.cs
@@ -3,6 +3,7 @@
 //action and func
 
 //events
+using ConsoleTests;
 using System.Text;
 
 internal class Program
@@ -10,15 +11,17 @@ internal class Program
     private static void Main()
     {
         Console.OutputEncoding = Encoding.UTF8;
+        var d = new Downloader();
+        d.Downloaded += (s, r) => Console.WriteLine(s);
+        d.OnDownladStart += a => Console.WriteLine(a);
+        d.OnDownladStart += a =>  SendMail("հաճախորդ@Մեյլ․ռու", a);
+        d.Downloaded += (a, r) =>  SendMail("հաճախորդ@Մեյլ․ռու", $"{a}, արդյունքը՝ {r}");
 
-        Func<string, int> f = s =>
-        {
-            int u = int.Parse(s);
-            int r = u * 2;
-            return r;
-        };
+        d.Download();
+    }
 
-        var result = f.Invoke("123");
-        Console.WriteLine(result);
+    static void SendMail(string email, string content)
+    {
+        Console.WriteLine($"{email} մեյլին ուղարկվել է {content}");
     }
 }
