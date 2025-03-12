3f3e67b492770342306153fa63387b1b676cc00b
Ruben Martirosyan <ruben@ruben.com>
2025-03-12 17:55:46 +0100

    Add delete

--- UniversityProgram.Mvc/Controllers/StudentController.cs
+++ UniversityProgram.Mvc/Controllers/StudentController.cs
@@ -5,36 +5,43 @@ namespace UniversityProgram.Mvc.Controllers
 {
     public class StudentController : Controller
     {
-        public IActionResult Index()
-        {
-            TempData["Student"] = "Մարտիրոս";
-            var result = new List<UserViewModel>()
+       static List<UserViewModel> users = new List<UserViewModel>()
             {
                 new UserViewModel()
                 {
                     Name = "Պողոս",
                     Age = 56,
+                    Email = "Poghso@gmail.com",
                     ImageUrl = "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
 ,                },
                 new UserViewModel()
                 {
                     Name = "Մարտիրոս",
                     Age = 23,
+                    Email = "Martiros@gmail.com",
+
                     ImageUrl = "https://plus.unsplash.com/premium_photo-1686245735403-a01147d86b89?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                 },
                 new UserViewModel()
                 {
                     Name = "Անուշ",
                     Age = 45,
+                    Email = "Anush@gmail.com",
+
                     ImageUrl = "https://images.unsplash.com/photo-1741526798351-50eeb46b2a06?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                 },
                 new UserViewModel()
                 {
                     Name = "Բարդուղեմիոս",
+                    Email = "Bard@gmail.com",
                     Age = 34
                 }
             };
-            return View(result);
+        public IActionResult Index()
+        {
+            TempData["Student"] = "Մարտիրոս";
+   
+            return View(users);
         }
 
         public IActionResult New()
@@ -46,6 +53,26 @@ namespace UniversityProgram.Mvc.Controllers
         {
             var user = new UserViewModel() { Age = 20, Name = "Բարդուղեմիոս" };
             return View("User", user);
+        }
+
+        public IActionResult StudentCreate()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Delete(string email)
+        {
+            var user = users.First(x => x.Email == email);
+            users.Remove(user);
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Create(UserViewModel model)
+        {
+            users.Add(model);
+            return RedirectToAction("Index");
         }
     }
 }
--- UniversityProgram.Mvc/Models/UserViewModel.cs
+++ UniversityProgram.Mvc/Models/UserViewModel.cs
@@ -1,10 +1,15 @@
-﻿namespace UniversityProgram.Mvc.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniversityProgram.Mvc.Models
 {
     public class UserViewModel
     {
         public string Name { get; set; }
 
         public int Age { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
 
         public string ImageUrl { get; set; }
     }
