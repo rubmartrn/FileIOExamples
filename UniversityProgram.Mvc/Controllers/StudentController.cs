b044a853e01d034727f8489c7f59ac7278b3fa1c
Ruben Martirosyan <ruben@ruben.com>
2025-03-12 16:34:17 +0100

    Add some action methods

---
+++ UniversityProgram.Mvc/Controllers/StudentController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UniversityProgram.Mvc.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("New");
+        }
+
+        public IActionResult New()
+        {
+            return View();
+        }
+    }
+}
