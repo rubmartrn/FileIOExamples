93177ff8748ec804c3e303e2bbec06409f1c2dad
Ruben Martirosyan <ruben@ruben.com>
2025-03-12 17:25:52 +0100

    Add table in student page

--- UniversityProgram.Mvc/Controllers/StudentController.cs
+++ UniversityProgram.Mvc/Controllers/StudentController.cs
@@ -8,13 +8,38 @@ namespace UniversityProgram.Mvc.Controllers
         public IActionResult Index()
         {
             TempData["Student"] = "Մարտիրոս";
-            return View();
+            var result = new List<UserViewModel>()
+            {
+                new UserViewModel()
+                {
+                    Name = "Պողոս",
+                    Age = 56,
+                    ImageUrl = "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
+,                },
+                new UserViewModel()
+                {
+                    Name = "Մարտիրոս",
+                    Age = 23,
+                    ImageUrl = "https://plus.unsplash.com/premium_photo-1686245735403-a01147d86b89?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                },
+                new UserViewModel()
+                {
+                    Name = "Անուշ",
+                    Age = 45,
+                    ImageUrl = "https://images.unsplash.com/photo-1741526798351-50eeb46b2a06?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                },
+                new UserViewModel()
+                {
+                    Name = "Բարդուղեմիոս",
+                    Age = 34
+                }
+            };
+            return View(result);
         }
 
         public IActionResult New()
         {
-            TempData["StudentName"] = "Պողոս";
-            return RedirectToAction(nameof(Index));
+            return View(8);
         }
 
         public IActionResult UserPage()
