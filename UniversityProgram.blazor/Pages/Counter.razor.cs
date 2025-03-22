eba3f17f65c57ae5b487740be43184d33317f2f6
Ruben Martirosyan <rubmartrn@gmail.com>
2025-03-22 10:57:01 +0100

    Add blazor.css

---
+++ UniversityProgram.blazor/Pages/Counter.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace UniversityProgram.blazor.Pages
+{
+    public partial class Counter
+    {
+
+        [Parameter]
+        public string UserName { get; set; }
+
+        [Parameter]
+        public EventCallback<string> OnChanged { get; set; }
+
+        private int currentCount = 0;
+
+        private async Task IncrementCount()
+        {
+            await OnChanged.InvokeAsync("տեքտս");
+            UserName = "Բարև Ձեզ";
+            currentCount++;
+        }
+    }
+}
