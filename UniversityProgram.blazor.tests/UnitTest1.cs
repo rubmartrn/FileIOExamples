6e858b22e30df347b47c73fed1daccdf92c12c33
Ruben Martirosyan <rubmartrn@gmail.com>
2025-03-24 18:55:19 +0100

    Add unit test for blazor

---
+++ UniversityProgram.blazor.tests/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using Bunit;
+using UniversityProgram.blazor.Pages;
+
+namespace UniversityProgram.blazor.tests
+{
+    public class UnitTest1 : TestContext
+    {
+        [Fact]
+        public void Test1()
+        {
+            // Arrange
+            var component = RenderComponent<NewPage>();
+            var button = component.Find(".myButton");
+
+            var textComponent = component.Find("#myText");
+
+            // Act
+            button.Click();
+
+            // Assert
+            textComponent.MarkupMatches("<p id=\"myText\">user name: Պողոս</p>");
+        }
+    }
+}
