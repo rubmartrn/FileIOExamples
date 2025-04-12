29d977d52a05b7a6e5bf8147cae257de870ab104
Ruben Martirosyan <ruben@ruben.com>
2025-04-12 11:50:35 +0200

    Use IdentityDb context and usermanagement

---
+++ NewAuthTestApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NewAuthTestApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost("login")]
+        public async Task Login([FromBody] UserModel model, [FromServices] SignInManager<IdentityUser> mng)
+        {
+            var result = await mng.PasswordSignInAsync(model.Email, model.Password, false, false);
+        }
+    }
+
+    public class UserModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ NewAuthTestApi/Program.cs
@@ -0,0 +1,55 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<IdentityDbContext>(e => e.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=IdentityTest"));
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(e =>
+{
+    e.User.RequireUniqueEmail = true;
+    e.Password.RequiredLength = 5;
+    e.Password.RequireDigit = true;
+    e.Password.RequireLowercase = true;
+    e.Password.RequireUppercase = true;
+})
+.AddEntityFrameworkStores<IdentityDbContext>()
+.AddDefaultTokenProviders();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapGet("/seed", async (ctx) =>
+{
+    var db = ctx.RequestServices.GetRequiredService<IdentityDbContext>();
+    db.Database.EnsureCreated();
+    var mng = ctx.RequestServices.GetRequiredService<UserManager<IdentityUser>>();
+    var user = new IdentityUser()
+    {
+        UserName = "testuser",
+        Email = "ruben@gmail.com"
+    };
+
+    var result = await mng.CreateAsync(user, "Aa%12345");
+    Console.Write("a");
+});
+
+app.MapControllers();
+
+app.Run();
