0b1d242f2dc71cc09870389673f133201ab619ad
Ruben Martirosyan <ruben@ruben.com>
2025-04-16 18:32:25 +0200

    Auth requests from front to api

---
+++ BlazorWASM/Handlers/CustomAuthMessageHandler.cs
@@ -0,0 +1,28 @@
+﻿
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using System.Net.Http.Headers;
+
+namespace BlazorWASM.Handlers
+{
+    public class CustomAuthMessageHandler : DelegatingHandler
+    {
+        private readonly IAccessTokenProvider _provider;
+
+        public CustomAuthMessageHandler(IAccessTokenProvider provider)
+        {
+            _provider = provider;
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var tokenResult = await _provider.RequestAccessToken();
+            if (tokenResult.TryGetToken(out var token))
+            {
+                request.Headers.Authorization =
+                    new AuthenticationHeaderValue("Bearer", token.Value);
+            }
+
+            return await base.SendAsync(request, cancellationToken);
+        }
+    }
+}
--- BlazorWASM/Program.cs
+++ BlazorWASM/Program.cs
@@ -1,4 +1,5 @@
 using BlazorWASM;
+using BlazorWASM.Handlers;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -6,12 +7,21 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5042") });
+
+builder.Services.AddScoped<CustomAuthMessageHandler>();
+builder.Services.AddHttpClient("ServerAPI",
+      client => client.BaseAddress = new Uri("http://localhost:5042"))
+    .AddHttpMessageHandler<CustomAuthMessageHandler>();
+
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>()
+  .CreateClient("ServerAPI"));
 
 builder.Services.AddOidcAuthentication(options =>
 {
     builder.Configuration.Bind("Auth0", options.ProviderOptions);
     options.ProviderOptions.ResponseType = "code";
+
+    options.ProviderOptions.AdditionalProviderParameters.Add("audience", builder.Configuration["Auth0:Audience"]!);
 });
 
 await builder.Build().RunAsync();
