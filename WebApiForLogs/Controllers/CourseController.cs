48b7785a9477a4fcd5f523e0726e4df644031bff
Ruben Martirosyan <ruben@ruben.com>
2025-04-28 18:06:36 +0200

    Add logging

---
+++ WebApiForLogs/Controllers/CourseController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiForLogs.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CourseController : ControllerBase
+    {
+        private readonly ILogger<CourseController> _logger;
+
+        public CourseController(ILogger<CourseController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            _logger.Log(LogLevel.Information, "Get method called");
+            return Ok(new { Message = "Hello from CourseController" });
+        }
+    }
+}
---
+++ WebApiForLogs/Controllers/StudentController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiForLogs.Controllers.First
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StudentController : ControllerBase
+    {
+        private readonly ILogger<StudentController> _logger;
+
+        public StudentController(ILogger<StudentController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            _logger.LogInformation("Get method called");
+            return Ok(new { Message = "Hello from StudentController" });
+        } 
+        
+        [HttpDelete]
+        public IActionResult Delete()
+        {
+            _logger.LogWarning("Delete method called");
+            return Ok(new { Message = "Hello from StudentController" });
+        }
+
+        [HttpPost]
+        public IActionResult Post()
+        {
+            _logger.LogError("Delete method called");
+            return Ok(new { Message = "Hello from StudentController" });
+        }
+    }
+}
