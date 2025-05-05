f5d7ecc1e7d2ef8004393a4eb24538a307d0607a
Ruben Martirosyan <ruben@ruben.com>
2025-05-05 17:16:38 +0200

    Add webapi for app insights

---
+++ WepApiForAppIns/Controllers/StudentController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WepApiForAppIns.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { Message = "Hello from StudentController" });
+        }
+
+        [HttpGet("error")]
+        public IActionResult Error()
+        {
+            throw new ArgumentOutOfRangeException(nameof(Error));
+        }
+    }
+}
