ac36864cd091d66d97c3bb669bc604a7af0ef006
Ruben Martirosyan <ruben@ruben.com>
2025-05-07 18:50:48 +0200

    Add project for caching

---
+++ CachingApi/Controllers/StudentController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UniversityProgram.BLL.Services;
+
+namespace CachingApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StudentController : ControllerBase
+    {
+        private readonly IStudentService _studentService;
+
+        public StudentController(IStudentService studentService)
+        {
+            _studentService = studentService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get(CancellationToken token)
+        {
+            var students = await _studentService.GetAll(token);
+            return Ok(new { Message = "Hello from StudentController" });
+        }
+    }
+}
---
+++ CachingApi/Program.cs
@@ -0,0 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using UniversityProgram.BLL.Services;
+using UniversityProgram.Data;
+using UniversityProgram.Data.Repositories;
+using UniversityProgram.Domain.BaseRepositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IStudentService, StudentService>();
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+
+builder.Services.AddDbContext<StudentDbContext>(options =>
+    options.UseSqlServer(builder.Configuration["studentDb"]));
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
