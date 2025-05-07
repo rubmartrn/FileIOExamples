685547beae614e9e5a8d1f9d3d9ef5c1f5e2079a
Ruben Martirosyan <ruben@ruben.com>
2025-05-07 17:18:45 +0200

    Remove external dependencies

--- WepApiForAppIns/Controllers/StudentController.cs
+++ WepApiForAppIns/Controllers/StudentController.cs
@@ -24,12 +24,6 @@ namespace WepApiForAppIns.Controllers
             return Ok(new { Message = "Hello from StudentController" });
         }
 
-        [HttpGet("error")]
-        public IActionResult Error()
-        {
-            throw new ArgumentOutOfRangeException(nameof(Error));
-        }
-
         [HttpGet("students")]
         public async Task<IActionResult> GetStudents(CancellationToken token)
         {
@@ -46,28 +40,6 @@ namespace WepApiForAppIns.Controllers
                 return NotFound();
             }
             return Ok(student);
-        }
-
-        [HttpPost("students")]
-        public async Task<IActionResult> AddStudent([FromBody] StudentAddModel model, CancellationToken token)
-        {
-            await _service.Add(model, token);
-            return Ok();
-        }
-
-        [HttpGet("External")]
-        public async Task<IActionResult> GetExternal()
-        {
-            var students = await _client.GetAsync("http://localhost:5260/Students");
-            return Ok(students);
-        }
-
-        [HttpGet("google")]
-        public async Task<IActionResult> GetGoogle()
-        {
-            var response = await _client.GetAsync("https://www.google.com");
-            var content = await response.Content.ReadAsStringAsync();
-            return Ok(content);
         }
     }
 }
