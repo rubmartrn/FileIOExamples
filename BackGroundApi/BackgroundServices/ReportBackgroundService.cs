34bf1e0b28ebbca6f49349f30f6d8ab8a2c1cf57
Ruben Martirosyan <ruben@ruben.com>
2025-05-10 12:14:24 +0200

    Add timer in backgroundService

--- BackGroundApi/BackgroundServices/ReportBackgroundService.cs
+++ BackGroundApi/BackgroundServices/ReportBackgroundService.cs
@@ -12,21 +12,47 @@ namespace BackGroundApi.BackgroundServices
             _logger = logger;
         }
 
+        Timer? _timer;
+
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            while (true)
+            _timer = new Timer(StartWork, null, TimeSpan.Zero, TimeSpan.FromSeconds(5));
+        }
+
+        private void StartWork(object? state)
+        {
+            bool isRunning = false;
+            Task.Run(async () =>
             {
-                await Task.Delay(5000, stoppingToken);
-                _logger.LogError("Report started" + DateTime.Now);
-                await Task.Delay(2000);
-                Database.Reports.Add(new Report
+                try
                 {
-                    Name = "Report 1",
-                    UpdatedAt = DateTime.UtcNow,
-                    Information = "Report 1 information"
-                });
-                _logger.LogError("Report finished" + DateTime.Now);
-            }
+                    if (isRunning)
+                    {
+                        return;
+                    }
+                    isRunning = true;
+                    await DoWorkAsync();
+                    isRunning = false;
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, "Error in ReportBackgroundService");
+                }
+            });
+        }
+
+        private async Task DoWorkAsync()
+        {
+            await Task.Delay(5000);
+            _logger.LogError("Report started" + DateTime.Now);
+            await Task.Delay(2000);
+            Database.Reports.Add(new Report
+            {
+                Name = "Report 1",
+                UpdatedAt = DateTime.UtcNow,
+                Information = "Report 1 information"
+            });
+            _logger.LogError("Report finished" + DateTime.Now);
         }
     }
 }
