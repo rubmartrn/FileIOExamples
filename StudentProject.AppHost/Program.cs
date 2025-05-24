817e18f5884f818ac37c14ca4f70d87d40ddefbd
Ruben Martirosyan <ruben@ruben.com>
2025-05-24 11:53:49 +0200

    Add azure functions

--- StudentProject.AppHost/Program.cs
+++ StudentProject.AppHost/Program.cs
@@ -6,4 +6,6 @@ builder.AddProject<Projects.BackGroundApi>("backgroundapi");
 
 builder.AddProject<Projects.Netflix_Rental_Jobs>("netflix-rental-jobs");
 
+builder.AddAzureFunctionsProject<Projects.FunctionApp1>("functionapp1");
+
 builder.Build().Run();
